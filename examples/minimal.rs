d817504f0c81c006ce98101123a03e96aecbde7a
Oleg Buss <olegbuss@gmail.com>
2022-11-22 04:28:53 +0100

    Update to Bevy 0.9 (#42)
    * Update to Bevy 0.9-dev
    
    * - apply bevy_mod_raycast changelog for 0.5.0
    - update dependencies
    
    * remove unnecessary qualifier
    
    * formatting and 0.9 syntax
    
    Co-authored-by: Oleg Buss <oleg.buss@dswiss.com>
    Co-authored-by: Aevyrie Roessler <aevyrie@gmail.com>

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -1,17 +1,23 @@
 use bevy::{prelude::*, window::PresentMode::AutoNoVsync};
+use bevy_mod_picking::DefaultPickingPlugins;
+use bevy_transform_gizmo::TransformGizmoPlugin;
 
 fn main() {
     App::new()
-        .insert_resource(WindowDescriptor {
-            present_mode: AutoNoVsync,
-            ..Default::default()
-        })
         .insert_resource(Msaa { samples: 4 })
-        .add_plugins(DefaultPlugins)
-        .add_plugins(bevy_mod_picking::DefaultPickingPlugins)
-        .add_plugin(bevy_transform_gizmo::TransformGizmoPlugin::new(
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                present_mode: AutoNoVsync,
+                ..Default::default()
+            },
+            ..default()
+        }))
+        .add_plugins(DefaultPickingPlugins)
+        .add_plugin(TransformGizmoPlugin::new(
             Quat::from_rotation_y(-0.2), // Align the gizmo to a different coordinate system.
-        )) // Use TransformGizmoPlugin::default() to align to the scene's coordinate system.
+                                         // Use TransformGizmoPlugin::default() to align to the
+                                         // scene's coordinate system.
+        ))
         .add_startup_system(setup)
         .run();
 }
@@ -23,35 +29,38 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // plane
-    commands
-        .spawn_bundle(PbrBundle {
+    commands.spawn((
+        PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
             material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
             ..Default::default()
-        })
-        .insert_bundle(bevy_mod_picking::PickableBundle::default())
-        .insert(bevy_transform_gizmo::GizmoTransformable);
+        },
+        bevy_mod_picking::PickableBundle::default(),
+        bevy_transform_gizmo::GizmoTransformable,
+    ));
     // cube
-    commands
-        .spawn_bundle(PbrBundle {
+    commands.spawn((
+        PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
             transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..Default::default()
-        })
-        .insert_bundle(bevy_mod_picking::PickableBundle::default())
-        .insert(bevy_transform_gizmo::GizmoTransformable);
+        },
+        bevy_mod_picking::PickableBundle::default(),
+        bevy_transform_gizmo::GizmoTransformable,
+    ));
     // light
-    commands.spawn_bundle(PointLightBundle {
+    commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..Default::default()
     });
     // camera
-    commands
-        .spawn_bundle(Camera3dBundle {
+    commands.spawn((
+        Camera3dBundle {
             transform: Transform::from_xyz(2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
-        })
-        .insert_bundle(bevy_mod_picking::PickingCameraBundle::default())
-        .insert(bevy_transform_gizmo::GizmoPickSource::default());
+        },
+        bevy_mod_picking::PickingCameraBundle::default(),
+        bevy_transform_gizmo::GizmoPickSource::default(),
+    ));
 }
