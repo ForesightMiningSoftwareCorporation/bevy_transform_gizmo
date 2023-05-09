ac39f1dda372306b3e9b286efa5242ebdcb8ac2e
KnstAnt <knstantna@gmail.com>
2023-05-10 02:50:01 +0300

    modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -656,8 +656,8 @@ fn process_new_transformable (
 
 fn process_new_camera (
     mut commands: Commands,
-//    new_transformable_camera: Query<Entity, Added<GizmoPickSource>>,    
-    new_transformable_camera: Query<Entity, (With<GizmoPickSource>, With<Camera>, Without<bevy_mod_picking::prelude::RaycastPickCamera>)>,   
+    new_transformable_camera: Query<Entity, Added<GizmoPickSource>>,    
+//    new_transformable_camera: Query<Entity, (With<GizmoPickSource>, With<Camera>, Without<bevy_mod_picking::prelude::RaycastPickCamera>)>,   
 ) {
     for entity in new_transformable_camera.iter() {
         if let Some(mut entity_commands) = commands.get_entity(entity) {
